77172c554e58f4ad6fcc32eaac4eac8f942dcbaf
Kudr_ie20 <Kudr_ie20@itstepru.onmicrosoft.com>
2022-07-10 18:39:14 +0300

    database created!!!

---
+++ app/src/main/java/com/example/basedatosneco1/User.java
@@ -0,0 +1,18 @@
+package com.example.basedatosneco1;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class User {
+    public String id, name, sec_name, email;
+
+
+    public User() {
+    }
+
+    public User(String id, String name, String sec_name, String email) {
+        this.id = id;
+        this.name = name;
+        this.sec_name = sec_name;
+        this.email = email;
+    }
+}
